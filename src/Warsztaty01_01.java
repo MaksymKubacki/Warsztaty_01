6afc103c6d169bbbc138a69719fccfd14cff06ed
Maksym Kubacki <maksym.kubacki@gmail.com>
2017-11-10 14:03:42 +0100

    first commit

---
+++ src/Warsztaty01_01.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Warsztaty01_01 {
+
+	public static void main(String[] args) {
+
+		Random los = new Random();
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Zgadnij liczbę:");
+		if(scan.nextLine() != los);
+		
+		
+		
+	}
+
+}
